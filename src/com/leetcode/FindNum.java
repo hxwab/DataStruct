f6dfab15e37330874489e7b12f75bf361e3268e3
hxwab <hxwab2010@126.com>
2016-01-25 21:20:50 +0800

    add

---
+++ src/com/leetcode/FindNum.java
@@ -0,0 +1,9 @@
+package com.leetcode;
+/**
+ * 在由N个正数的集合S中:找出最大元素C,满足C=A+B,其中A,B都是集合S中元素.请给出算法描述、代码与时间复杂度分析
+ * @author huangxw
+ *
+ */
+public class FindNum {
+
+}
---
+++ src/com/leetcode/Queue2Stack.java
@@ -0,0 +1,10 @@
+package com.leetcode;
+/**
+ * 两个 队列实现一个栈
+ * @author huangxw
+ *http://www.cnblogs.com/kaituorensheng/archive/2013/03/02/2939690.html
+ */
+public class Queue2Stack {
+	
+
+}
