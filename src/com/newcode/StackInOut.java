df26805ce9117e797e9d2c8df33131abd0aebef0
hxwab <hxwab2010@126.com>
2016-02-25 16:27:27 +0800

    add

---
+++ src/com/newcode/StackInOut.java
@@ -0,0 +1,37 @@
+package com.newcode;
+
+import java.util.Stack;
+
+/**
+ * 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。假设压入栈的所有数字均不相等。
+ * 例如序列1,2,3,4,5是某栈的压入顺序，序列4，5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+ * @author csdc
+ *
+ */
+public class StackInOut {
+
+	
+	
+	public boolean isInOrder(int [] pushA , int[] popB){
+		Stack<Integer> stack = new Stack<Integer>();
+		int index =0;
+		for(int  item :pushA){
+		    stack.push(item);
+			if(item==popB[index]){
+				stack.pop();
+				index++;
+			}
+		}
+		
+		while(!stack.isEmpty()){
+			if(stack.peek()==popB[index]){
+				stack.pop();
+				index++;
+			}else {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
