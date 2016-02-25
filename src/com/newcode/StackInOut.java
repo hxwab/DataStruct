f9ba59085f2326c16e202a859e7cb68d0bbe103c
hxwab <hxwab2010@126.com>
2016-02-25 16:34:32 +0800

    add

--- src/com/newcode/StackInOut.java
+++ src/com/newcode/StackInOut.java
@@ -10,9 +10,15 @@ import java.util.Stack;
  */
 public class StackInOut {
 
-	
+	public static void main(String[] args) {
+		boolean result =new StackInOut().isInOrder(new int[]{1,2,3,4,5}, new int[]{4,5,3,2,1});
+		 result =new StackInOut().isInOrder(new int[]{1,2,3,4,5}, new int[]{4,3,5,2,1});
+		
+		System.out.println(result);
+	}
 	
 	public boolean isInOrder(int [] pushA , int[] popB){
+		if(pushA.length==0) return false; //要考虑为空的情况
 		Stack<Integer> stack = new Stack<Integer>();
 		int index =0;
 		for(int  item :pushA){
