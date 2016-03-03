767bd3591f2d02a252a65358faf6551dfd38b1b6
hxwab <hxwab2010@126.com>
2016-03-03 10:23:43 +0800

    add

--- src/com/datastruct/sort/Buket.java
+++ src/com/datastruct/sort/Buket.java
@@ -2,7 +2,7 @@ package com.datastruct.sort;
 /**
  *桶排序:前提是所有元素都小于N,类似于bitmap的映射，把i放到数组下表为i的位置。时间复杂度O(M+N),空间复杂度O(N)
  * @author csdc
- *
+ *若有负数，则找出最小的负数，加上之前的值使之成为非负数。输出时所有的值都减去这个数
  */
 public class Buket  extends Sort{
 
