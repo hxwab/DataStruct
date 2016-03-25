160e98432884af3a66c3d3023abd2887949d1bc1
hxwab <hxwab2010@126.com>
2016-03-25 23:17:34 +0800

    add

--- src/com/newcode/link/FindMid.java
+++ src/com/newcode/link/FindMid.java
@@ -1,5 +1,49 @@
 package com.newcode.link;
 
+import java.util.Random;
+import com.newcode.link.Link.Node;
+
+/**
+ * 获取链表的中间元素，若有偶数个则取前面的那一个
+ * 思想：快慢指针，时间复杂度o(N)
+ *   
+ */
 public class FindMid {
 
+	private static Link  link = new Link() ;
+	
+	public static void main(String[] args) {
+		
+		FindMid f =new FindMid();
+		f.creat();
+		link.display();
+		System.out.println(f.getMid());
+		
+	}
+	
+	
+	public int getMid(){
+		Node fast =link.getFirstNode() ;
+		Node slow = link.getFirstNode();
+		
+		while(true){
+			fast = fast.next;
+			if(fast==null) break;
+			fast =fast.next;
+			if (fast==null) break;
+			slow = slow.next;
+		}
+		return slow.vaule;
+	}
+	
+	
+	/**
+	 * 随机生成链表
+	 */
+	public void creat(){
+		Random r = new Random();
+		for(int i=0;i<9;i++){
+			link.insert(r.nextInt(50));
+		}
+	}
 }
