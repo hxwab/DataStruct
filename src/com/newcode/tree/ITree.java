9a8dc4d742b3f14e85ce534e8502b4552d9b762a
hxwab <hxwab2010@126.com>
2016-06-22 08:58:30 +0800

    add

--- src/com/newcode/tree/ITree.java
+++ src/com/newcode/tree/ITree.java
@@ -4,6 +4,14 @@ import com.newcode.tree.Tree.TreeNode;
 
 public interface ITree {
 	
+	public void insert(TreeNode node);
+	
+	public void delete(TreeNode node);
+	
+	public boolean isTreeNode(int  val);
+	
+	
+	
 	public boolean isBalance(TreeNode root);
 	
 	public int getDepth(TreeNode root);
